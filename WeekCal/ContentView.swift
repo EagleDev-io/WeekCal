4d6144cb2be9dc4d7a6fa3ea9ff21315aeaadaab
Daniel Esteban Cardona Rojas <drojas@rei.com>
2021-01-02 16:16:40 -0500

    Initial Commit

---
+++ WeekCal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeekCal
+//
+//  Created by Daniel Rojas on 2/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
